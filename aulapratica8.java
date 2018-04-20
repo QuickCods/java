17f6c4a05693f6d786e7040d49870f6b9cf716fc
QuickCods <35273049+QuickCods@users.noreply.github.com>
2018-04-20 09:41:07 +0100

    Create aulapratica8.java

---
+++ aulapratica8.java
@@ -0,0 +1,23 @@
+/*
+execicio 1
+
+a) 2n^3 >> 5n^2
+   g(n) >> f(n)
+   f(n) pertente O(g(n))
+
+b) f e g têm o mesmo grau, por isso exsitem constantes que g pode majorar ou minorar f
+   f(n) pertence a teta, omega e O de g(n)
+
+c) 2^n >> 6n^2
+   f(n) pertence ao omega de g(n)
+   
+d) f e g são constantes e de grau zero
+   f(n) pertence a teta, omega e O de g(n)
+
+e) nLog(n) << n^2
+   f(n) pertence ao O(g(n))
+
+f) sqrt(n) >> log(n)
+   f(n) pertence ao omega de g(n)
+
+*/
